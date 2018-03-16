040af05d66a834f596ab575f92b7b02dbeefa8fa
郭凯峰 <guokaifeng@guokaifengdeMacBook-Pro.local>
2018-03-16 20:58:05 +0800

    wepy 的 redux 学习

---
+++ wepy/wepyxdemo/src/store/types/counter.js
@@ -0,0 +1,9 @@
+export const INCREMENT = 'INCREMENT'
+
+export const DECREMENT = 'DECREMENT'
+
+export const ASYNC_INCREMENT = 'ASYNC_INCREMENT'
+
+export const UP_NUM = 'UP_NUM'
+
+export const UP_ORDER = 'UP_ORDER'
