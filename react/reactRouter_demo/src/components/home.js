77c9a45e7d03e95a6fd37df8e4c23a575be47091
郭凯峰 <guokaifeng@guokaifengdeMacBook-Pro.local>
2018-03-23 21:08:33 +0800

    react-router

--- react/reactRouter_demo/src/components/home.js
+++ react/reactRouter_demo/src/components/home.js
@@ -3,6 +3,8 @@ import React,{Component} from 'react';
 
 class Home extends Component {
     render(){
+        console.log(this.props.location);
+        
        return (
             <div className='home'>
                 <h1>Home</h1>
--- react/reactRouter_demo/src/index.js
+++ react/reactRouter_demo/src/index.js
@@ -3,20 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import registerServiceWorker from './lib/registerServiceWorker';
-// import { Router } from 'react-router';
-// import Home from './components/home';
-// import About from './components/about'
-// import Inbox from './components/inbox'
-
-// const routes = {
-//     path: '/',
-//     component: App,
-//     indexRoute: { component: Home },
-//     childRoutes: [
-//       { path: 'about', component: About },
-//       { path: 'inbox', component: Inbox },
-//     ]
-//   }
+// import ParamsExample from './app/paramsExample'
 
 ReactDOM.render(
     <App/>,
