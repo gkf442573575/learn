7a2852be565f502db5b92803f056166e8edb4608
郭凯峰 <guokaifeng@guokaifengdeMacBook-Pro.local>
2018-04-01 16:57:51 +0800

    react-router

--- react/reactRouter_demo/src/components/about.js
+++ react/reactRouter_demo/src/components/about.js
@@ -3,6 +3,21 @@ import React,{Component} from 'react';
 
 
 class About extends Component {
+   
+
+    goHome = ()=>{
+        console.log(this.props.history);
+        let history = this.props.history;
+        // history.push('/inbox');
+
+        history.replace({
+            pathname:'/inbox',
+            state:{
+                from:'1'
+            }
+        })
+    }
+
     render(){
         let props = this.props,
             match = props.match,
@@ -10,6 +25,7 @@ class About extends Component {
         return (
             <div className = 'about'>
                 <h1>About {params.id}</h1>
+                <button onClick={this.goHome}>点击</button>
             </div>
         )
     }
--- react/reactRouter_demo/src/components/inbox.js
+++ react/reactRouter_demo/src/components/inbox.js
@@ -1,6 +1,8 @@
 import React, { Component} from 'react';
 class Inbox extends Component {
     render (){
+        console.log('state',this.props.location.state);
+        
         return (
             <div className='indox'>
                 <h1>Indox</h1>
