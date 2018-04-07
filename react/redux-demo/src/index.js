819cec675280fe6d7bce3dcf825e03cae1a00954
郭凯峰 <guokaifeng@guokaifengdeMacBook-Pro.local>
2018-04-07 20:08:04 +0800

    redux

---
+++ react/redux-demo/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    createStore
+} from 'redux';
+import {
+    Provider
+} from 'react-redux';
+import './index.css';
+import rootReducer from './reducers'
+import App from './components/App';
+import registerServiceWorker from './lib/registerServiceWorker';
+
+
+const store = createStore(rootReducer)
+ReactDOM.render( 
+    <Provider store={store}>
+        < App / > 
+    </Provider>, 
+    document.getElementById('root')
+);
+registerServiceWorker();
