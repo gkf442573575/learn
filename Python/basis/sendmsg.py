1053d7800c8ef82cc4d1369733d7146d16b8231f
郭凯峰 <guokaifeng@guokaifengdeMacBook-Pro.local>
2018-05-06 21:45:21 +0800

    python

---
+++ Python/basis/sendmsg.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+
+# 定义模块
+
+def test1():
+    print('test1')
+
+    
+def test2():
+    print('test2')
+
+
+
+# 模块自执行测试
+if __name__ == '__main__':
+    test1()
+    test2()
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
