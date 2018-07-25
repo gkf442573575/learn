9dc8a466a7eeecada3d312cb78ae0dc1ddcf4d0b
gkf442573575 <gkf442573575@163.com>
2018-07-25 17:01:01 +0800

    代码整理

--- react/react_shelf/src/pages/index/index.jsx
+++ react/react_shelf/src/pages/index/index.jsx
@@ -5,22 +5,20 @@ import { NavBar, Icon } from "antd-mobile";
 import "./index.less";
 
 class Index extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  goback = a => {
-      
-  };
-  render() {
-    return (
-      <div id="index">
-        <NavBar mode="light" onLeftClick={this.goback.bind(this, 1)}>
-          首页
-        </NavBar>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    goback = a => {};
+    render() {
+        return (
+            <div id="index">
+                <NavBar mode="light" onLeftClick={this.goback.bind(this, 1)}>
+                    首页
+                </NavBar>
+            </div>
+        );
+    }
 }
 
 export default Index;
