a94c0d24c255fc8c900373e4870376c7a7a6d861
gkf <gkf442573575@163.com>
2019-08-19 09:37:41 +0800

    nest 1

---
+++ node/nest/nest_1/src/cats/cats.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Get,
+  Put,
+  Post,
+  Delete,
+  Req,
+  HttpCode,
+  Header,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { CreateCatDto, UpdateCatDto } from '../dots/cats.dto';
+import { Cat } from '../interfaces/cat.interface';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  @HttpCode(204)
+  @Header('Cache-Control', 'none')
+  async create(@Body() createCatDto: CreateCatDto){
+    this.catsService.create(createCatDto);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<string> {
+    return `这是${id}的猫`;
+  }
+
+  @Get()
+  async findAll(@Req() request): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateCatDto: UpdateCatDto,
+  ): Promise<string> {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<string> {
+    return `This action removes a #${id} cat`;
+  }
+}
