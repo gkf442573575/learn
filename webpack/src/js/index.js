e92a93b4b15016a9b98adc6c0baa9102ee744230
guokaifeng <gkf442573585@163.com>
2021-11-10 16:37:49 +0800

    remove not use

--- webpack/src/js/index.js
+++
@@ -1,7 +0,0 @@
-import '../css/index.scss';
-
-import app from '@/utils/app';
-
-
-console.log(app.id);
-console.log(2);
--- wepy/wepyxdemo/src/store/types/counter.js
+++
@@ -1,9 +0,0 @@
-export const INCREMENT = 'INCREMENT'
-
-export const DECREMENT = 'DECREMENT'
-
-export const ASYNC_INCREMENT = 'ASYNC_INCREMENT'
-
-export const UP_NUM = 'UP_NUM'
-
-export const UP_ORDER = 'UP_ORDER'
